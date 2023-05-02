bde5e8a627aab3a1a29a4c091d9f10c51f56b473
ahmedatef286 <89429256+ahmedatef286@users.noreply.github.com>
2023-05-02 12:29:43 +0300

    initial

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -7,5 +7,9 @@ class Style {
 class _colorPalette {
 //write your colors here
   static Color activeOrange = Color.fromARGB(255, 227, 123, 100);
-  static Color backgroundGrey = Color(0x262837);
+  static Color backgroundBlue1 = Color.fromARGB(255, 38, 40, 55);
+  static Color backgroundBlue2 = Color.fromARGB(255, 31, 29, 44);
+  static Color brightWhite = Color.fromARGB(255, 252, 251, 253);
+  static Color midWhite = Color.fromARGB(255, 224, 222, 225);
+  static Color dimWhite = Color.fromARGB(255, 187, 185, 188);
 }
