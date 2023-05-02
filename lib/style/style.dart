3b07b8eb90ef7bd640ef0777cbb9c9f077f7b862
ahmedatef286 <89429256+ahmedatef286@users.noreply.github.com>
2023-05-02 14:38:36 +0300

    organizing

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomStyle {
-  static final _colorPalette colorPalette = colorPalette;
+  static final _colorPalette colorPalette = _colorPalette();
 }
 
 class _colorPalette {
@@ -14,12 +14,9 @@ class _colorPalette {
   Color dimWhite = Color.fromARGB(255, 187, 185, 188);
 }
 
-class _FontSizes {
-//write your colors here
-  Color activeOrange = Color.fromARGB(255, 227, 123, 100);
-  Color backgroundBlue1 = Color.fromARGB(255, 38, 40, 55);
-  Color backgroundBlue2 = Color.fromARGB(255, 31, 29, 44);
-  Color brightWhite = Color.fromARGB(255, 252, 251, 253);
-  Color midWhite = Color.fromARGB(255, 224, 222, 225);
-  Color dimWhite = Color.fromARGB(255, 187, 185, 188);
+class _fontSizes {
+  //write common font size here
+  double miniSize = 14.0;
+  double medium = 20.0;
+  double large = 30;
 }
